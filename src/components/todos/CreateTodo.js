a86feeda68e7e16e2f8f50960002151e60b0e61e
Hanna Lee <mani21na@gmail.com>
2020-04-25 08:08:46 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -18,7 +18,7 @@ class CreateTodo extends Component {
   render() {
     return(
       <div>
-        <form>
+        <form onSubmit={ event => this.handleSubmit(event) }>
           <p>
             <label>add todo</label>
             <input type="text" onChange={ this.handleChange } value={ this.state.text } />

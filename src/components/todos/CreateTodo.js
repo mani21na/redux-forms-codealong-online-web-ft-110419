f79549f14e748bc1ded70fcdbce1fac73f6121db
Hanna Lee <mani21na@gmail.com>
2020-04-25 08:09:06 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -15,6 +15,11 @@ class CreateTodo extends Component {
     });
   };
 
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+  }
+
   render() {
     return(
       <div>

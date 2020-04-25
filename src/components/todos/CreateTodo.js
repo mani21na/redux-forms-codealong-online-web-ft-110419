758d97e9ffd962b203f657e2d41aa63543517cc0
Hanna Lee <mani21na@gmail.com>
2020-04-25 07:39:59 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    }
+  }
+
   render() {
     return(
       <div>
